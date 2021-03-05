42cc9333ed2ac33e6da419b74632001d7bb77e45
Erick Ortiz <eortiz@canvia.com>
2021-03-05 17:39:53 -0500

    .

--- infra.tf
+++ infra.tf
@@ -45,15 +45,33 @@ resource "azurerm_network_security_group" "secgrouppubprod" {
   }
 }
 
-resource "azurerm_network_ddos_protection_plan" "ddospubprod" {
+resource "azurerm_virtual_network" "vnprod" {
   count = "${length(var.bussiness_units_short)}"
-  name                = "${var.bussiness_units_short[count.index]}pub${local.production-short}Ddos"
-  location            = azurerm_resource_group.rgcompanybuprod[count.index].location
-  resource_group_name = azurerm_resource_group.rgcompanybuprod[count.index].name
+  name                = "${var.bussiness_units_short[count.index]}${local.production-short}virtualNetwork"
+  location            = azurerm_resource_group.azurerm_resource_group.rgcompanybuprod[count.index].location
+  resource_group_name = azurerm_resource_group.azurerm_resource_group.rgcompanybuprod[count.index].name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "publicsubnet"
+    address_prefix = "10.0.1.0/24"
+    security_group = azurerm_network_security_group.secgrouppubprod[count.index].id
+  }
+
+  subnet {
+    name           = "privatesubnet1"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  subnet {
+    name           = "privatesubnet2"
+    address_prefix = "10.0.3.0/24"
+  }
 
   tags = {
     Ambiente = "${local.production}"
-    Area = "${var.bussiness_units[count.index]}"
+    Area = "${local.bussiness-unit-1}"
     Empresa = "${local.company}"
   }
 }
