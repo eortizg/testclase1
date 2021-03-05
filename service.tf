8644152f8bbe5b9da8a3eebb3174c9fa7df547cc
Erick Ortiz <eortiz@canvia.com>
2021-03-05 18:06:31 -0500

    .

--- service.tf
+++ service.tf
@@ -1,7 +1,3 @@
-provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  features {}
-}
 
 resource "azurerm_resource_group" "rgdev" {
   name     = local.service-rg-dev
