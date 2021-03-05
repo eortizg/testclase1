d777b18e61ed9256b8767e66a86d1953be779ca9
Erick Ortiz <eortiz@canvia.com>
2021-03-05 17:02:52 -0500

    .

--- variables.tf
+++ variables.tf
@@ -25,7 +25,7 @@ variable "bussiness_units_short" {
   type        = list
   description = "List of Business units short for company"
   default = [
-    "cupl",
+    "cupl2",
     "comp",
     "usrn",
     "prov"
