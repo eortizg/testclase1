cf376c7c9d38369b923193e9a680ba0d4c96b64d
Erick Ortiz <eortiz@canvia.com>
2021-03-05 16:49:24 -0500

    .

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "mandatory_tag_keys" {
+  type        = list
+  description = "List of mandatory tag keys used by policies 'addTagToRG','inheritTagFromRG','bulkAddTagsToRG','bulkInheritTagsFromRG'"
+  default = [
+    "Ambiente",
+    "Area",
+    "Empresa"
+  ]
+}
+
+
+variable "bussiness_units" {
+  type        = list
+  description = "List of Business units for company"
+  default = [
+    "Cumplimiento",
+    "Compras",
+    "UsuariosDeNegocio",
+    "Proveedores"
+  ]
+}
+
+
+variable "bussiness_units_short" {
+  type        = list
+  description = "List of Business units short for company"
+  default = [
+    "cupl",
+    "comp",
+    "usrn",
+    "prov"
+  ]
+}
